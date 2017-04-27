9ba335c52a7bfc6e31d9dcc31f9ee6e51ffe8582
Eric D. Reichert <eric@reichertconsulting.com>
2017-04-27 14:58:56 -0400

    Added the map to the map pane.

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -1,16 +1,21 @@
 package controllers
 
 import javax.inject._
+
+import play.api.Configuration
 import play.api.mvc._
 import play.api.i18n._
+import utils.ConfigKeys
 
 @Singleton
 class IndexController @Inject() (
     val messagesApi: MessagesApi,
-    implicit val wja: WebJarAssets
+    implicit val wja: WebJarAssets,
+    configuration: Configuration
 ) extends Controller with I18nSupport {
 
   def index = Action { implicit request =>
-    Ok(views.html.index())
+    // TODO: Handle the option correctly.
+    Ok(views.html.index(configuration.getString(ConfigKeys.mapsUrl).get))
   }
 }
---
+++ app/utils/ConfigKeys.scala
@@ -0,0 +1,6 @@
+package utils
+
+object ConfigKeys {
+
+  final lazy val mapsUrl = "minotaur.mapsUrl"
+}
